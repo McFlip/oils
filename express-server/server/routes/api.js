2a21ef07ba5aa48cb587226cc66e0547646f420b
McFlip <gradydenton@yahoo.com>
2018-11-08 16:18:00 -0600

    Update MongoDB connection

--- express-server/server/routes/api.js
+++ express-server/server/routes/api.js
@@ -7,9 +7,12 @@ import * as UserController from '../controllers/user.js';
 
 // MongoDB URL from the docker-compose file
 const dbHost = 'mongodb://database/mean-docker';
+const dbOpts = { useNewUrlParser: true };
 
 // Connect to mongodb
-mongoose.connect(dbHost);
+mongoose.connect(dbHost, dbOpts)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 /* GET api listing. */
 router.get('/', UserController.ping);
