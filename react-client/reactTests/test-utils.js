baf55e4b82696fede8b324ae52f190721d583ebf
McFlip <gradydenton@yahoo.com>
2018-11-17 21:12:53 -0500

    added testing framework

---
+++ react-client/reactTests/test-utils.js
@@ -0,0 +1,11 @@
+import {render} from 'react-testing-library';
+import {createStore} from 'redux';
+import {Provider, connect} from 'react-redux';
+
+
+
+// re-export everything
+export * from 'react-testing-library';
+
+// override render method
+//export {renderWithRedux as render};
