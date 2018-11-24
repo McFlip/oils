1682c3ea7bcab181adf5082757c7a5a9e0efe528
McFlip <gradydenton@yahoo.com>
2018-11-23 22:19:35 -0600

    cleanup

--- react-client/src/components/prods_list.js
+++ react-client/src/components/prods_list.js
@@ -36,19 +36,11 @@ class ProdsList extends Component {
 
   handleClose(){
     this.setState({sel_prod: null});
-<<<<<<< HEAD
-=======
-    console.log('closing');
->>>>>>> 0dcb9185385143d531e3510804571bab2bc2b9da
   }
 
   handleSubmit(id, value){
     this.props.updateProd(id, value);
     this.handleClose();
-<<<<<<< HEAD
-=======
-
->>>>>>> 0dcb9185385143d531e3510804571bab2bc2b9da
   }
 
   renderProds() {
