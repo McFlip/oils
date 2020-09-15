466efb312cbae40e45aaf91db83c41b4b5b105b6
McFlip <gradydenton@yahoo.com>
2020-09-15 00:58:58 -0400

    uprade nginx & use tls; DRY compose yml

--- react-client/build.bash
+++ react-client/build.bash
@@ -4,4 +4,9 @@
 cp .env.production .env
 docker build -t mcflip/oils -f Dockerfile-production .
 docker push mcflip/oils
+
+# copy config to be uploaded to server
+cp ../docker-compose-nginx.yml ../dist
+
+# reset env
 cp .env.dev .env
