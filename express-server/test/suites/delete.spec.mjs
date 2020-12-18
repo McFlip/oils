ce393b4a8b03d4d4490f1c0d8816274b80ca7186
McFlip <gradydenton@yahoo.com>
2020-12-18 14:56:22 -0500

    del prod

--- express-server/test/suites/delete.spec.mjs
+++ express-server/test/suites/delete.spec.mjs
@@ -2,8 +2,11 @@
 /* eslint-disable no-console */
 /* eslint-env mocha, node */
 import chai from 'chai'
+import fs from 'fs'
 
 export default function del () {
+  let image4ID = null
+
   it('should delete post image', function (done) {
     const { apiURL, prod1ID, post1ID, image2ID } = this.test.ctx
     const post = {
@@ -52,6 +55,73 @@ export default function del () {
     const { apiURL, image3ID } = this.test.ctx
     chai.request(apiURL)
       .get(`/images/${image3ID}`)
+      .end((err, res) => {
+        if (err) console.log(err)
+        res.should.have.status(404)
+        done()
+      })
+  })
+  it('should delete the second product', function (done) {
+    const { apiURL, token, prod2ID, use1ID } = this.test.ctx
+    const post = {
+      'title': '1st product post',
+      'content': 'new post',
+      'id': prod2ID,
+      'deleteImg': false
+    }
+    // Set up a post and use to test cascade delete
+    chai.request(apiURL)
+      .post(`/uses/${use1ID}/product/${prod2ID}`)
+      .set({ Authorization: `Bearer ${token}` })
+      .then(() => {
+        chai.request(apiURL)
+          .post('/posts')
+          .type('form')
+          .set({ Authorization: `Bearer ${token}` })
+          .attach('image', fs.readFileSync('test/data/favicon.ico'), 'deleteMe.ico')
+          .field(post)
+          .then(() => {
+            // get the image ID of the new post
+            chai.request(apiURL)
+              .get(`/products/${prod2ID}`)
+              .set({ Authorization: `Bearer ${token}` })
+              .then((res) => {
+                image4ID = res.body.posts[0].image
+              })
+              .then(() => {
+                // finally delete the product
+                chai.request(apiURL)
+                  .delete(`/products/${prod2ID}`)
+                  .set({ Authorization: `Bearer ${token}` })
+                  .then((res) => {
+                  // console.log(res.text)
+                    res.text.should.eq(prod2ID)
+                    done()
+                  })
+              })
+          })
+      })
+      .catch((err) => {
+        console.log(err)
+        done()
+      })
+  })
+  it('should confirm 1st recipe no longer contains 2nd prod as ingredient', function (done) {
+    const { apiURL, recipe1ID } = this.test.ctx
+    chai.request(apiURL)
+      .get(`/recipes/${recipe1ID}`)
+      .end((err, res) => {
+        if (err) console.log(err)
+        // console.log(res.body.ingredients)
+        res.body.ingredients.length.should.eq(1)
+        res.body.ingredients[0].product.descr.should.eq('first test product')
+        done()
+      })
+  })
+  it('should confirm that the post image was deleted', function (done) {
+    const { apiURL } = this.test.ctx
+    chai.request(apiURL)
+      .get(`/images/${image4ID}`)
       .end((err, res) => {
         if (err) console.log(err)
         res.should.have.status(404)
